fb2e1f40cb00dd88bd7676de72e37a02fe6bfcb6
Henry <110545703+kiwayu@users.noreply.github.com>
2025-09-24 23:06:00 +0100

    Adjusted File structure and Dark mode

--- obj/Debug/net9.0-windows/ToDoApp.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows/ToDoApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2ac140eb267513668cb226596cf4f4a49ac4513")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
